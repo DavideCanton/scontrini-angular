993592a1758976f9bfc5159f9636a067406cd5ce
Davide Canton <davide.canton5@gmail.com>
2018-04-24 12:48:48 +0200

    modifiche test

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
-    },
+    },    
     angularCli: {
       environment: 'dev'
     },
